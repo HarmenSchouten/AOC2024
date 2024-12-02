5f385c871b6215a8773844626d6cb975fd0a029e
Harmen Schouten <harmenschouten@outlook.com>
2024-12-02 08:32:09 +0100

    Day 01

--- Day01/Part1.ts
+++ Day01/Part1.ts
@@ -1,1 +1,19 @@
-const input = await Deno.readTextFile("/Day01/input.txt");
+const input = await Deno.readTextFile("./Day01/input.txt");
+
+const items = input
+    .split("\r\n")
+    .map(line => {
+        const [left, right] = line.split("   ").map(Number)
+
+        return {
+            left: left,
+            right: right
+        }
+    })
+
+const left = items.map(item => item.left).sort((a,b) => a - b)
+const right = items.map(item => item.right).sort((a,b) => a - b)
+
+const answer = left.reduce((acc, state, idx) => acc + Math.abs(state - right[idx]), 0)
+
+console.log(answer)
