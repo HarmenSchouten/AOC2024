12abb6ec159d3578eecd4ed21f668d2a866d8288
Harmen Schouten <harmenschouten@outlook.com>
2024-12-02 08:32:14 +0100

    Day 02

--- Day02/Part1.ts
+++ Day02/Part1.ts
@@ -1,1 +1,20 @@
-const input = await Deno.readTextFile("/Day02/input.txt");
+const input = await Deno.readTextFile("./Day02/input.txt");
+
+const isLineSafe = (items: number[]) => {
+    return items.every((item, idx, arr) => {
+        if (idx === 0) return true;
+        const diff = item - arr[idx - 1]
+        return diff < 0 && Math.abs(diff) <= 3
+    }) || items.every((item, idx, arr) => {
+        if (idx === 0) return true;
+        const diff = item - arr[idx - 1]
+        return diff > 0 && Math.abs(diff) <= 3
+    })
+}
+
+const answer = input
+    .split("\r\n")
+    .map(line => line.split(" ").map(Number))
+    .reduce((acc, state) => isLineSafe(state) ? acc += 1 : acc, 0)   
+
+console.log(answer)
--- Day02/Part2.ts
+++ Day02/Part2.ts
@@ -1,1 +1,24 @@
-const input = await Deno.readTextFile("/Day02/input.txt");
+const input = await Deno.readTextFile("./Day02/input.txt");
+
+const isLineSafe = (items: number[], isDecimated?: boolean): boolean => {
+    return items.every((item, idx, arr) => {
+        if (idx === 0) return true;
+        const diff = item - arr[idx - 1]
+        return diff < 0 && Math.abs(diff) <= 3
+    }) || items.every((item, idx, arr) => {
+        if (idx === 0) return true;
+        const diff = item - arr[idx - 1]
+        return diff > 0 && Math.abs(diff) <= 3
+    }) || (!isDecimated && items.some((_, idx, arr) => {
+        const copy = [...arr]
+        copy.splice(idx, 1)
+        return isLineSafe(copy, true)
+    }))
+}
+
+const answer = input
+    .split("\r\n")
+    .map(line => line.split(" ").map(Number))
+    .reduce((acc, state) => isLineSafe(state) ? acc += 1 : acc, 0)   
+
+console.log(answer)
