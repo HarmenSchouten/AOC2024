ace1a6c82aa45909aea53b7731af350dae6832a4
Harmen Schouten <harmenschouten@outlook.com>
2024-12-03 10:47:58 +0100

    Day 03

--- Day03/Part1.ts
+++ Day03/Part1.ts
@@ -1,1 +1,12 @@
-const input = await Deno.readTextFile("/Day03/input.txt");
+const input = await Deno.readTextFile("./Day03/input.txt");
+
+const validItems = input.match(/(mul\(\d*,\d*\))/g) ?? []
+
+const answer = validItems
+    .map(item => item.slice(4, item.length - 1).split(",").map(Number))
+    .reduce((acc, state) => {
+        if (state.length != 2) return acc;
+        return acc += (state[0] * state[1])
+    }, 0)   
+
+console.log(answer)
