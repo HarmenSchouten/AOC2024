fda52fb09f5a639f26a0c16287865e5b3219a367
Harmen Schouten <harmenschouten@florinco.nl>
2024-12-03 10:47:58 +0100

    Day 03

--- Day03/Part2.ts
+++ Day03/Part2.ts
@@ -1,1 +1,19 @@
-const input = await Deno.readTextFile("/Day03/input.txt");
+const input = await Deno.readTextFile("./Day03/input.txt");
+
+const validItems = input.match(/(mul\(\d*,\d*\))|(do\(\))|(don't\(\))/g) ?? []
+
+let enabled = true;
+
+const answer = validItems
+    .reduce((acc, state) => {
+        if (state.startsWith("don't")) enabled = false;
+        if (state.startsWith("do()")) enabled = true;
+        if (enabled) {
+            const numbers = state.slice(4, state.length - 1).split(",").map(Number)
+            if (numbers.length != 2) return acc;
+            acc += (numbers[0] * numbers[1])
+        }
+        return acc;
+    }, 0)   
+
+console.log(answer)
