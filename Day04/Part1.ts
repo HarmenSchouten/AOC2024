116fe2b2b8c1a99fcb16c6c84734c619ff2cff6e
Harmen Schouten <harmenschouten@outlook.com>
2024-12-04 10:01:39 +0100

    Day 04

--- Day04/Part1.ts
+++ Day04/Part1.ts
@@ -4,45 +4,35 @@ const input = await Deno.readTextFile("./Day04/input.txt");
 
 const grid = new Grid(input, (v) => String(v))
 
-const xCells = grid.cells.filter(item => item.value === "X")
-
-const answer = xCells
-    .map(cell => {
+const answer = grid.cells
+    .filter(item => item.value === "X")
+    .flatMap(cell => {
         const neighbours = cell.getAllNeighbours(false, item => item.value === "M")
-        return {
-            x: cell,
-            m: neighbours
-        }
+        return neighbours.map(item => ({ 
+            xDiff: item.x - cell.x,
+            yDiff: item.y - cell.y,
+            x: cell, 
+            m: item 
+        }))
     })
-    .map(cellXM => {
-        return cellXM.m
-            .flatMap(item => {
-                const xDiff = item.x - cellXM.x.x;
-                const yDiff = item.y - cellXM.x.y;
+    .filter(item => item.m !== undefined)
+    .flatMap(cellXM => {
+        const neighbours = cellXM.m.getAllNeighbours(false, item => 
+            item.value === "A"
+            && item.x === cellXM.m.x + cellXM.xDiff
+            && item.y === cellXM.m.y + cellXM.yDiff)
+        return neighbours.map(item => ({ ...cellXM, a: item}))
+    })
+    .filter(item => item.a !== undefined)
+    .flatMap(cellXMA => {
+        const neighbours = cellXMA.a.getAllNeighbours(false, item => 
+            item.value === "S"
+            && item.x === cellXMA.a.x + cellXMA.xDiff
+            && item.y === cellXMA.a.y + cellXMA.yDiff)
 
-                return {
-                    xDiff: xDiff,
-                    yDiff: yDiff,
-                    x: cellXM.x,
-                    m: item,
-                    a: grid.cells.find(c => c.x === item.x + xDiff && c.y === item.y + yDiff && c.value === "A")             
-                }
-            })
-            .filter(item => item.a !== undefined)
+        return neighbours.map(item => ({...cellXMA, s: item}))
     })
-    .map(cells => {
-        return cells
-            .flatMap(xma => {
-                return {
-                    ...xma,
-                    s: grid.cells.find(c => c.x === xma.a!.x + xma.xDiff && c.y === xma.a!.y + xma.yDiff && c.value === "S")
-                }
-            
-            })
-            .filter(item => item.s !== undefined)
-    })
-    .flatMap(combinations => combinations.map(c => `${c.x.toString()} ${c.m.toString()} ${c.a!.toString()} ${c.s!.toString()}`))
-    .filter((item, idx, arr) => arr.indexOf(item) === idx)
-    .length
+    .filter(item => item.s !== undefined)
+    .length   
 
 console.log(answer)
