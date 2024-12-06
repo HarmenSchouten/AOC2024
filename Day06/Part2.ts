3f5330a291ce9e5f555aec389691f74427ed9067
Harmen Schouten <harmenschouten@outlook.com>
2024-12-06 12:22:35 +0100

    Optimized

--- Day06/Part2.ts
+++ Day06/Part2.ts
@@ -1,26 +1,27 @@
-import { Grid } from "../utils/grid.ts";
 import "../utils/index.ts"
 const input = await Deno.readTextFile("./Day06/input.txt");
 
-const grid = new Grid(input, v => String(v))
-const start = grid.cells.find(item => item.value === "^")!;
+const grid = input.splitLb().map(line => line.split(""))
+const startY = grid.findIndex(y => y.includes("^"))!
+const startX = grid[startY].indexOf("^")
 
 const runRoute = (x?: number, y?: number) => {
+    
     const shouldDetectLoop = x !== undefined && y !== undefined
-
+    
     if (shouldDetectLoop) {
-        grid.updateCell(x, y, "#")
+        grid[y][x] = "#"
     }
-
+    
     let currentDirection = "U"
     const visitedPositions = new Set<string>()
-    let currentPosition = grid.cells.find(item => item.value === "^")!
-
-    while (currentPosition !== undefined) {
-
+    let currentPosition = [startX, startY]
+    
+    while (grid[currentPosition[1]]?.[currentPosition[0]]) {
+        
         const key = shouldDetectLoop
-            ? `${currentPosition.x}, ${currentPosition.y}, ${currentDirection}`   
-            : currentPosition.toString()
+            ? `${currentPosition[0]}, ${currentPosition[1]}, ${currentDirection}`   
+            : `${currentPosition[0]}, ${currentPosition[1]}, ${grid[currentPosition[1]][currentPosition[0]]}`
 
         if (shouldDetectLoop && visitedPositions.has(key)) {
             break;
@@ -31,29 +32,29 @@ const runRoute = (x?: number, y?: number) => {
         switch (currentDirection) {
             case "U":
                 {
-                    const newPosition = currentPosition.up();                
-                    if (newPosition?.value === "#") currentDirection = "R"
+                    const newPosition = [currentPosition[0], currentPosition[1] - 1]            
+                    if (grid?.[newPosition[1]]?.[newPosition[0]] === "#") currentDirection = "R"
                     else currentPosition = newPosition!
                     break;
                 }
             case "R":
                 {
-                    const newPosition = currentPosition.right();
-                    if (newPosition?.value === "#") currentDirection = "D"
+                    const newPosition = [currentPosition[0] + 1, currentPosition[1]]
+                    if (grid?.[newPosition[1]]?.[newPosition[0]] === "#") currentDirection = "D"
                     else currentPosition = newPosition!
                     break;
                 }
             case "D":
                 {
-                    const newPosition = currentPosition.down();
-                    if (newPosition?.value === "#") currentDirection = "L"
+                    const newPosition = [currentPosition[0], currentPosition[1] + 1]
+                    if (grid?.[newPosition[1]]?.[newPosition[0]] === "#") currentDirection = "L"
                     else currentPosition = newPosition!
                     break;
                 }
             case "L":
                 {
-                    const newPosition = currentPosition.left();
-                    if (newPosition?.value === "#") currentDirection = "U"
+                    const newPosition = [currentPosition[0] - 1, currentPosition[1]]
+                    if (grid?.[newPosition[1]]?.[newPosition[0]] === "#") currentDirection = "U"
                     else currentPosition = newPosition!
                     break;
                 }
@@ -61,7 +62,7 @@ const runRoute = (x?: number, y?: number) => {
     }
 
     if (shouldDetectLoop) {
-        grid.updateCell(x, y, ".")
+        grid[y][x] = "."
     }
 
     return {
@@ -75,10 +76,10 @@ const { visitedPositions } = runRoute()
 const answer = [...visitedPositions.entries()]
     .map(([item]) => item.split(", ").slice(0, 2).map(Number))
     .reduce((acc, [x, y]) => {
-        const isStart = x === start.x && y === start.y
+        const isStart = x === startX && y === startY
         if (isStart) return acc;
 
-        const isWall = grid.cells.find(cell => cell.x === x && cell.y === y)?.value === "#"
+        const isWall = grid[y][x] === "#"
         if (isWall) return acc;
 
         if (runRoute(x, y).isLooping) {
