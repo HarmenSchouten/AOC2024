accd40bfda183e975b91c76d9c1f802408700f05
Harmen Schouten <harmenschouten@outlook.com>
2024-12-07 09:04:32 +0100

    Day 07

--- Day07/Part1.ts
+++ Day07/Part1.ts
@@ -1,1 +1,33 @@
-const input = await Deno.readTextFile("/Day07/input.txt");
+import '../utils/index.ts'
+const input = await Deno.readTextFile("./Day07/input.txt");
+
+const equationLines = input
+    .splitLb()
+    .map(item => {
+        const [result, numbersStr] = item.split(": ")
+
+        return {
+            result: Number(result),
+            numbers: numbersStr.split(" ").map(Number)
+        }
+    })
+
+const answer = equationLines.reduce((sum, line) => {
+
+    const possibleResults = line.numbers.reduce((acc, state) => {
+        if (acc.length === 0) return [state]
+
+        return acc.flatMap(nr => [
+            nr + state,
+            nr * state
+        ])
+    }, [] as number[])
+    
+    if (possibleResults.includes(line.result)) {
+        return sum += line.result
+    }
+
+    return sum;
+}, 0)
+
+console.log(answer)
