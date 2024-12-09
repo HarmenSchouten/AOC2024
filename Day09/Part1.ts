124bfca576c5a6d79f424f3bdf8b36984125669f
Harmen Schouten <harmenschouten@outlook.com>
2024-12-09 11:20:55 +0100

    Day 09

--- Day09/Part1.ts
+++ Day09/Part1.ts
@@ -1,1 +1,41 @@
-const input = await Deno.readTextFile("/Day09/input.txt");
+import "../utils/index.ts"
+const input = await Deno.readTextFile("./Day09/input.txt");
+
+const parsed = input
+    .split("")
+    .map((item, idx) => {
+        const isFile = idx % 2 === 0
+
+        return {
+            isFile: isFile,
+            index: isFile ? idx / 2 : undefined,
+            value: Array.from({length: Number(item)}).map(_ => isFile ? `${idx === 0 ? 0 : idx / 2}` : ".")
+        }
+    })
+
+while (true) {
+
+    const firstGapIndex = parsed.findIndex(item => !item.isFile && item.value.length > 0)
+    if (firstGapIndex == -1) break;
+    
+    const gap = parsed[firstGapIndex]
+    const lastFileIdx = parsed.findLastIndex(item => item.isFile && item.value.length > 0)
+    
+    if (lastFileIdx == -1 || lastFileIdx < firstGapIndex) break;
+    
+    const lastFile = parsed[lastFileIdx]
+    const numberToReplace = lastFile.index
+    
+    gap.value.splice(gap.value.indexOf("."), 1, `${numberToReplace}`)
+    lastFile.value = lastFile.value.slice(0, -1)
+
+    if (lastFile.value.length === 0) lastFile.isFile = false;
+    if (!gap.value.includes(".")) gap.isFile = true
+}
+
+const checksum = parsed
+    .flatMap(item => item.value)
+    .filter(item => item != '.')
+    .reduce((acc, state, idx) => acc += (Number(state) * idx), 0)
+
+console.log(checksum)
--- Day09/Part2.ts
+++ Day09/Part2.ts
@@ -1,1 +1,44 @@
-const input = await Deno.readTextFile("/Day09/input.txt");
+import "../utils/index.ts"
+const input = await Deno.readTextFile("./Day09/input.txt");
+
+const parsed = input
+    .split("")
+    .map((item, idx) => {
+        const isFile = idx % 2 === 0
+
+        return {
+            index: idx,
+            isFile: isFile,
+            id: isFile ? idx / 2 : undefined,
+            value: Array.from({length: Number(item)}).map(_ => isFile ? `${idx === 0 ? 0 : idx / 2}` : ".")
+        }
+    })
+
+parsed
+    .filter(item => item.isFile)
+    .sort((a, b) => b.id! - a.id!)
+    .forEach((state) => {
+        const firstAvailableGap = parsed.find(item => 
+            !item.isFile 
+            && item.index < state.index
+            && item.value.filter(v => v === ".").length >= state.value.length)
+
+        if (!firstAvailableGap) return;
+        
+        const firstDotIndex = firstAvailableGap.value.indexOf(".")
+        firstAvailableGap.value.splice(firstDotIndex, state.value.length, ...state.value)
+
+        const item = parsed.find(item => item.id === state.id)!
+        item.value = item.value.map(_ => ".")
+        item.isFile = false
+    })
+
+
+const checksum = parsed
+    .flatMap(item => item.value)
+    .reduce((acc, state, idx) => {
+        if (state === '.') return acc;
+        return acc += (Number(state) * idx)
+    }, 0)
+
+console.log(checksum)
