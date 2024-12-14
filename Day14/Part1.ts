86f92695ce33e58fac13673914c1b2b775a8da26
Harmen Schouten <harmenschouten@outlook.com>
2024-12-14 12:47:12 +0100

    Day 14

--- Day14/Part1.ts
+++ Day14/Part1.ts
@@ -1,1 +1,44 @@
-const input = await Deno.readTextFile("/Day14/input.txt");
+import "../utils/index.ts"
+const input = await Deno.readTextFile("./Day14/input.txt");
+
+const gridWidth = 101;
+const gridHeight = 103;
+const seconds = 100;
+
+const bots = input
+    .splitLb()
+    .map(item => {
+        const [px, py, vx, vy] = item.match(/-*\d+/g)!.map(Number)
+        return {
+            position: { x: px, y: py },
+            velocity: { x: vx, y: vy }
+        }
+    })
+    .reduce((acc, bot) => {
+        const newX = bot.position.x + (bot.velocity.x * seconds)
+        const posX = newX - Math.floor(newX / gridWidth) * gridWidth
+
+        const newY = (bot.position.y + (bot.velocity.y * seconds))
+        const posY = newY - Math.floor(newY / gridHeight) * gridHeight
+
+        acc.push({x: posX, y: posY})
+        return acc;
+    }, [] as {x: number, y: number}[])
+
+const q1 = bots.filter(item => 
+    item.x >= 0 && item.x < Math.floor(gridWidth / 2)
+    && item.y >= 0 && item.y < Math.floor(gridHeight / 2)).length
+
+const q2 = bots.filter(item => 
+    item.x >= Math.ceil(gridWidth / 2) && item.x <= gridWidth
+    && item.y >= 0 && item.y < Math.floor(gridHeight / 2)).length
+
+const q3 = bots.filter(item => 
+    item.x >= 0 && item.x < Math.floor(gridWidth / 2)
+    && item.y >= Math.ceil(gridHeight / 2) && item.y <= gridHeight).length
+
+const q4 = bots.filter(item => 
+    item.x >= Math.ceil(gridWidth / 2) && item.x <= gridWidth
+    && item.y >= Math.ceil(gridHeight / 2) && item.y <= gridHeight).length 
+
+console.log(q1 * q2 * q3 * q4)
--- Day14/Part2.ts
+++ Day14/Part2.ts
@@ -1,1 +1,48 @@
-const input = await Deno.readTextFile("/Day14/input.txt");
+import "../utils/index.ts"
+const input = await Deno.readTextFile("./Day14/input.txt");
+
+const gridWidth = 101;
+const gridHeight = 103;
+
+const bots = input
+    .splitLb()
+    .map(item => {
+        const [px, py, vx, vy] = item.match(/-*\d+/g)!.map(Number)
+        return {
+            position: { x: px, y: py },
+            velocity: { x: vx, y: vy }
+        }
+    })
+
+let botsCopy = bots.slice()
+let i = 0;
+while (true) {
+    botsCopy = botsCopy.map(bot => {
+        let newX = bot.position.x + bot.velocity.x
+        if (newX > gridWidth) newX -= gridWidth
+        if (newX < 0) newX += gridWidth
+        
+        let newY = (bot.position.y + bot.velocity.y)
+        if (newY > gridHeight) newY -= gridHeight
+        if (newY < 0) newY += gridHeight 
+        return {...bot, position: {x: newX, y: newY}}
+    })
+    i++
+
+    const positions = new Set()
+    botsCopy.forEach(bot => positions.add(`${bot.position.x},${bot.position.y}`))
+    
+    if (positions.size === botsCopy.length) {
+        console.log(i)
+        for (let y = 0; y < gridHeight; y++) {
+            const items = []
+            for (let x = 0; x < gridWidth; x++) {
+                const hasBots = botsCopy.filter(bot => bot.position.x === x && bot.position.y === y)
+                if (hasBots.length > 0) items.push('#')
+                else items.push(" ")
+            }
+            console.log(items.join(""))
+        }
+        break;
+    }
+}
