ff26684899688b04a61e2952dc5977492268a3c2
Harmen Schouten <harmenschouten@outlook.com>
2024-12-22 12:43:32 +0100

    Day 22

--- Day22/Part1.ts
+++ Day22/Part1.ts
@@ -1,1 +1,45 @@
-const input = await Deno.readTextFile("/Day22/input.txt");
+import "../utils/index.ts"
+const input = await Deno.readTextFile("./Day22/input.txt");
+const numbers = input.splitLb().map(BigInt)
+
+const mul64 = (a: bigint) => a * BigInt(64)
+const mul2048 = (a: bigint) => a * BigInt(2048)
+const div = (a: bigint) => BigInt(Math.floor(Number(a) /32))
+const mix = (a: bigint, b: bigint) => a ^ b
+const mod = (a: bigint) => a % BigInt(16777216)
+
+const s1 = (input: bigint) => {
+    const b = mul64(input)
+    const c = mix(input, b)
+    return mod(c)
+}
+
+const s2 = (input: bigint) => {
+    const b = div(input)
+    const c = mix(input, b)
+    return mod(c)
+}
+
+const s3 = (input: bigint) => {
+    const b = mul2048(input)
+    const c = mix(input, b)
+    return mod(c)
+}
+
+const exec = (input: bigint) => {
+    let number = BigInt(input)
+    for (let i = 0; i < 2000; i++){
+        const resS1 = s1(number)
+        const resS2 = s2(resS1)
+        number = s3(resS2)
+    }
+    return number    
+}
+
+const answer = numbers.reduce((acc, state) => {
+    const x = exec(state)
+    console.log(x)
+    return acc += x
+}, 0n)
+
+console.log(answer)
--- Day22/Part2.ts
+++ Day22/Part2.ts
@@ -1,1 +1,59 @@
-const input = await Deno.readTextFile("/Day22/input.txt");
+import "../utils/index.ts"
+const input = await Deno.readTextFile("./Day22/input.txt");
+const numbers = input.splitLb().map(BigInt)
+
+const mul64 = (a: bigint) => a * BigInt(64)
+const mul2048 = (a: bigint) => a * BigInt(2048)
+const div = (a: bigint) => BigInt(Math.floor(Number(a) /32))
+const mix = (a: bigint, b: bigint) => a ^ b
+const mod = (a: bigint) => a % BigInt(16777216)
+
+const s1 = (input: bigint) => {
+    const b = mul64(input)
+    const c = mix(input, b)
+    return mod(c)
+}
+
+const s2 = (input: bigint) => {
+    const b = div(input)
+    const c = mix(input, b)
+    return mod(c)
+}
+
+const s3 = (input: bigint) => {
+    const b = mul2048(input)
+    const c = mix(input, b)
+    return mod(c)
+}
+
+const exec = (input: bigint) => {
+    const resS1 = s1(input)
+    const resS2 = s2(resS1)
+    return s3(resS2)
+}
+
+const ranges: { [key: string]: number[] } = {};
+numbers.forEach(number => {
+    let seed = number;
+    const visited = new Set<string>();
+    const changes: number[] = [];
+    for (let i = 0; i < 2000; i++) {
+        const next = exec(seed);
+        changes.push(Number((next % 10n) - (seed % 10n)));
+        seed = next;
+
+        if (changes.length === 4) {
+            const key = changes.join(',');
+            if (!visited.has(key)) {
+                if (ranges[key] === undefined) ranges[key] = [];
+                ranges[key].push(Number((next % 10n)));
+                visited.add(key);
+            }
+            changes.shift();
+        }
+    }
+})
+
+const answer = Math.max(...Object.values(ranges).map(range => range.reduce((sum, num) => sum + num, 0)));
+
+console.log(answer)
