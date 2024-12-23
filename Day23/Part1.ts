8062b7c7acfa0fcfae6c6226de9dd53ec1118e2d
Harmen Schouten <harmenschouten@outlook.com>
2024-12-23 11:55:51 +0100

    Day 23

--- Day23/Part1.ts
+++ Day23/Part1.ts
@@ -1,1 +1,37 @@
-const input = await Deno.readTextFile("/Day23/input.txt");
+import "../utils/index.ts"
+const input = await Deno.readTextFile("./Day23/input.txt");
+
+const graph = {} as Record<string, Set<string>>
+input
+    .splitLb()
+    .forEach(c => {
+        const [left, right] = c.split("-")
+        if (!graph[left]) graph[left] = new Set<string>()
+        if (!graph[right]) graph[right] = new Set<string>()
+        graph[left].add(right)
+        graph[right].add(left)
+    })
+
+const threeConnect = new Set<string>()
+
+for (const node in graph) {
+    const adjacents = Array.from(graph[node])
+
+    for (let i = 0; i < adjacents.length; i++) {
+        for (let j = i + 1; j < adjacents.length; j++) {
+            const n1 = adjacents[i]
+            const n2 = adjacents[j]
+
+            if (graph[n1].has(n2)) {
+                const triangle = [node, n1, n2].sort()
+                threeConnect.add(triangle.join(','))
+            }
+        }
+    }
+}
+
+const result = Array
+    .from(threeConnect)
+    .filter(t => t.split(",").some(n => n.startsWith('t')))
+
+console.log(result.length)
--- Day23/Part2.ts
+++ Day23/Part2.ts
@@ -1,1 +1,45 @@
-const input = await Deno.readTextFile("/Day23/input.txt");
+import "../utils/index.ts"
+const input = await Deno.readTextFile("./Day23/input.txt");
+
+const graph = {} as Record<string, Set<string>>
+input
+    .splitLb()
+    .forEach(c => {
+        const [left, right] = c.split("-")
+        if (!graph[left]) graph[left] = new Set<string>()
+        if (!graph[right]) graph[right] = new Set<string>()
+        graph[left].add(right)
+        graph[right].add(left)
+    })
+
+const nodes = Object.keys(graph)
+const parties = [] as string[][]
+
+const algo = (r: string[], p: string[], x: string[]) => {
+    if (p.length === 0 && x.length === 0) {
+        if (r.length >= 1 && r.length <= Number.MAX_SAFE_INTEGER) {
+            parties.push(r)
+        }
+        return
+    } else {
+        const pivot = p.concat(x)[0]
+        const pNeighbours = graph[pivot]
+
+        for (const node of p.filter(n => !pNeighbours.has(n))) {
+            algo(
+                [...r, node],
+                p.filter(n => graph[node].has(n)),
+                p.filter(n => graph[node].has(n))
+            )
+            p = p.filter(n => n !== node)
+            x.push(node)
+        }
+    }
+}
+
+algo([], nodes, [])
+const largestClique = parties.reduce((max, clique) => (clique.length > max.length ? clique : max), [])
+const largestCliqueNames = largestClique.sort((a, b) => a.localeCompare(b))
+const result = largestCliqueNames.join(',')
+
+console.log(result)
